dc5442fbffc02c57b14a42cc1e31c515a75fa72b
pozniej-znajde-wolne-haslo <aniarafeld@o2.pl>
2023-01-31 13:34:17 +0100

    adds js

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const heroAndEnemy = ["Batman, The Joker", "Professor, Magneto", "Thor, Loki"];
+
+heroAndEnemy.push("Deacon Frost, Blade", "Doctor Doom, Fantastic 4", "Mandarin, Iron Man");
+
+console.log(heroAndEnemy);
